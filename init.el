4079376d2db64202428de77eca09fcd49180eb38
Alan Andersen <alanderr@gmail.com>
2019-07-30 00:09:50 -0600

    cleanup

--- init.el
+++ init.el
@@ -22,12 +22,15 @@
       default-fill-column 80         ; toggle wrapping text at the 80th character
       initial-scratch-message "Welcome in Emacs" ; print a default message in the empty scratch buffer opened at startup
       lexical-binding t              ; enable lexical-binding for this buffer instead of dynamic binding (extremely slow)
-      winner-mode 1                  ; restore previous window arrangements and buffer contents of those windows
-      global-auto-revert-mode t      ; auto-sync files when modified on disk
-      electric-pair-mode t           ; auto parentheses pairing
       show-paren-delay 0             ; no delay for highlighting paren in show-paren-mode
-      show-paren-mode 1              ; highlight pairing parentheses with cursor
       )
+
+
+;; enable built in modes
+(global-auto-revert-mode)      ; auto-sync files when modified on disk
+(winner-mode)                  ; restore previous window arrangements and buffer contents of those windows
+(electric-pair-mode)           ; auto parentheses pairing
+(show-paren-mode)              ; highlight pairing parentheses with cursor
 
 (require 'package)
 
@@ -46,12 +49,22 @@
 
 (require 'use-package)
 
+
 ;; Vim mode
 (use-package evil :ensure t
+  :init
+  ;; needed for evil-collection
+  (setq evil-want-keybinding nil       ; see https://github.com/emacs-evil/evil-collection/issues/60
+	evil-want-integration t)
   :config
   (evil-mode 1)
-  (setq-default evil-escape-delay 0.2)
-  ) ; evil config guide -- https://github.com/noctuid/evil-guide
+  (setq-default evil-escape-delay 0.2)) ; evil config guide -- https://github.com/noctuid/evil-guide
+
+;; set sane defaults in other modes that aren't evilified
+(use-package evil-collection :ensure t
+  :after evil
+  :config
+  (evil-collection-init)) ; i don't know how useful this is yet. `evil-collection-mode-list' to see list of affected modes
 
 ;; Minimal UI
 (scroll-bar-mode -1)
@@ -59,24 +72,19 @@
 (tooltip-mode    -1)
 (menu-bar-mode   -1)
 
-;; Theme
- (use-package doom-themes :ensure t :config (load-theme 'doom-city-lights t))
 
+;; Theme
+(use-package doom-themes :ensure t :config (load-theme 'doom-city-lights t))
+
+
+;; Improved help menu
 (use-package helpful :ensure t) ;; gives much more context/information/examples then the built in help
+
 
 ;; auto-complete/narrowing search frameworks
 (use-package ivy :ensure t) ; narrow completion framework - comes up in a minibuffer by default
 (use-package counsel :ensure t) ; auto-completion framework in buffers
 (use-package swiper :ensure t) ; a use of ivy to narrow down searches in a buffer
-
-;; key-bindings
-(use-package general :ensure t
-  :config
-  (general-define-key
-   "C-'" #'avy-goto-word-1
-   "C-/" #'swiper
-   )
-  )
 
 (use-package which-key :ensure t
   :config
@@ -87,11 +95,13 @@
 (use-package avy :ensure t
   :commands (avy-goto-word-1)) ; trigger loading of package when the command `avy-goto-word-1' is used
 
+
 ;; Multi-Term
 (use-package multi-term
   :ensure t
   :init
 (setq multi-term-program "/bin/bash"))
+
 
 ;; Ranger
 (use-package ranger 
@@ -99,8 +109,10 @@
   :init
   (setq ranger-show-hidden t))
 
+
 ;; Code commenting
 (use-package evil-nerd-commenter :ensure t)
+
 
 ;; Project management
 (use-package projectile
@@ -114,6 +126,7 @@
   :config
   (counsel-projectile-mode))
 
+
 ;; Workspaces
 (use-package perspective
   :ensure t
@@ -122,25 +135,34 @@
 (use-package persp-projectile
   :ensure t)
 
+
 ;; Surround
 (use-package evil-surround
   :ensure t
   :config
   (global-evil-surround-mode 1))
 
+
+;; Switch window quickly
 (use-package ace-window :ensure t
   :config
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 
+
+;; Jump-to lisp code features
 (use-package elisp-slime-nav :ensure t
   :config
   (elisp-slime-nav-mode 1))
 
+
+;; keybindings
 (use-package general :ensure t
   :config 
   (general-define-key
    "M-x" 'counsel-M-x
    "M-o" 'ace-window
+   "C-'" #'avy-goto-word-1
+   "C-/" #'swiper
    "C-h j" 'elisp-slime-nav-describe-elisp-thing-at-point
    "C-h f" #'helpful-callable
    "C-h F" #'helpful-function ;;
@@ -165,6 +187,8 @@
    "C-c p" 'paste-from-os
    "C-c y" 'copy-to-os
    "C-c x" 'cut-to-os
+   "C-c h" #'winner-undo
+   "C-c l" #'winner-redo
    "C-]"   'elisp-slime-nav-find-elisp-thing-at-point)
   (general-define-key
    :states '(normal visual insert emacs)
@@ -281,15 +305,6 @@
 ;; comment styleguide (# of semicolons and placement) -- https://www.gnu.org/software/emacs/manual/html_node/elisp/Comment-Tips.html
 
 
-
-
-;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;  Working on understanding setting up general correctly with spacemacs like bindings
-;;;;;;;;;;;;;;;;;;
-
-
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -297,7 +312,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helpful elisp-slime-nav ace-window multi-term doom-themes which-key counsel ivy evil general use-package))))
+    (evil-collection helpful elisp-slime-nav ace-window multi-term doom-themes which-key counsel ivy evil general use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
