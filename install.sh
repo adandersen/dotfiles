38ebd148452e801774b20ec5a3a492d433ac4185
Alan Andersen <alanderr@gmail.com>
2020-05-01 17:03:40 -0600

    modified

--- install.sh
+++ install.sh
@@ -10,14 +10,15 @@ mkdir -p ~/.config/nvim
 ln -f ./.config/nvim/init.vim ~/.config/nvim/init.vim
 
 echo "Setup other dot files, hard linking in $HOME"
-ln ./.ideavimrc ../.ideavimrc
-ln ./.Xmodmap ../.Xmodmap
-ln ./.bash_profile ../.bash_profile
-ln ./.bashrc ../.bashrc
-ln ./.gitconfig ../.gitconfig
-ln ./.tmux.conf ../.tmux.conf
-ln ./.inputrc ../.inputrc
-ln ./.xmodmaprc ../.xmodmaprc
+ln ./.ideavimrc ~/.ideavimrc
+ln ./.bash_profile ~/.bash_profile
+ln ./.bashrc ~/.bashrc
+ln ./.gitconfig ~/.gitconfig
+ln ./.tmux.conf ~/.tmux.conf
+ln ./.inputrc ~/.inputrc
+ln ./.Xmodmap ~/.Xmodmap
+ln ./.xmodmaprc ~/.xmodmaprc
+ln ./.xsession ~/.xsession
 
 echo "Setup AwesomeWM config"
 mkdir -p ~/.config
