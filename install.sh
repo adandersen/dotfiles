c9ef2d080107218e6df9e1c46ae01311ab0274a0
Alan Andersen <alanderr@gmail.com>
2020-05-01 17:04:22 -0600

    fix

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ ln ./.xsession ~/.xsession
 
 echo "Setup AwesomeWM config"
 mkdir -p ~/.config
-ln -s ~/.local/dotfiles/awesome ~/.config/awesome
+ln -s ~/.local/dotfiles/.config/awesome ~/.config/awesome
 
 echo "Setup i3lock-color (lock screen, dependency for betterlockscreen script)"
 sudo apt install pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
